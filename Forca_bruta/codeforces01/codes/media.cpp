aa357b22000091b2c00cbbc33020456895ef60da
nicollasprado <nicollasprado.profissional@gmail.com>
2024-12-04 22:51:56 -0300

    Continuação da questão de media codeforces - força bruta

--- Forca_bruta/codeforces01/codes/media.cpp
+++ Forca_bruta/codeforces01/codes/media.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 int fat(int num){
     int factorial = 1;
@@ -38,7 +39,7 @@ void quickSort(double *a, int left, int right){
 }
 
 void reverse(double *a, int arraySize){
-    int j = arraySize-1;  
+    int j = arraySize-1;
     for(int i=0; i < arraySize/2; i++){
         double aux = a[j];
         a[j] = a[i];
@@ -49,8 +50,9 @@ void reverse(double *a, int arraySize){
 
 int main(){
     int qtTests, chosenAvg;
-    for(int i=0; i < 10; i++){
-        std::cin >> qtTests >> chosenAvg;
+    std::vector<double> results;
+    int qtResults = 0;
+    while(std::cin >> qtTests >> chosenAvg){
         double testsRes[qtTests];
         for(int j=0; j < qtTests; j++){
             std::cin >> testsRes[j];
@@ -59,45 +61,26 @@ int main(){
         int qtSubgroups = fat(qtTests) / (fat(3) * fat(qtTests - 3));
         double averages[qtSubgroups];
         int avgsIndex = 0;
-        int g = 0;  int j = g+1;  int k = j+1;
-        for(; g < qtTests-1; g++){
-            for(; j < qtTests-1; j++){
-                for(; k < qtTests-1; k++){
-                    if(g != j && g != k && j != k){
-                        averages[avgsIndex] = (testsRes[i] + testsRes[j] + testsRes[k]) / 3.0;
-                        std::cout << "a= " << g << " b= " << j << " c= " << k << std::endl;
-                        avgsIndex++;
-                    }
-                }
-                if(g != j && g != k && j != k){
-                    averages[avgsIndex] = (testsRes[i] + testsRes[j] + testsRes[k]) / 3.0;
-                    std::cout << "a= " << g << " b= " << j << " c= " << k << std::endl;
+        // Geração das médias
+        for(int g = 0; g < qtTests; g++){
+            for(int j = g+1; j < qtTests; j++){
+                for(int k = j+1; k < qtTests; k++){
+                    averages[avgsIndex] = (testsRes[g] + testsRes[j] + testsRes[k]) / 3.0;
                     avgsIndex++;
                 }
-                k = j+1;
             }
-            if(g != j && g != k && j != k){
-                averages[avgsIndex] = (testsRes[i] + testsRes[j] + testsRes[k]) / 3.0;
-                std::cout << "a= " << g << " b= " << j << " c= " << k << std::endl;
-                avgsIndex++;
-            }
-            j = g+1;
         }
-        
-        // print
-        for(int l=0; l < qtTests; l++){
-            std::cout << "testsRes: " << std::setprecision(2) << testsRes[l] << std::endl;
-        }
-        quickSort(averages, 0, qtSubgroups); // Ordenará do menor pro maior o array das médias
-        reverse(averages, qtSubgroups); // Inverte o arr
-        for(int m=0; m < qtSubgroups; m++){
-            std::cout << std::setprecision(2) << averages[m] << std::endl;
-        }
-        for(int n=0; n < qtTests; n++){
-            std::cout << "testsRes: " << std::setprecision(2) << testsRes[n] << std::endl;
-        }
-        std::cout << std::setprecision(2) << averages[chosenAvg-1] << std::endl;
 
+        quickSort(averages, 0, qtSubgroups-1); // Ordenará do menor pro maior o array das médias
+        reverse(averages, qtSubgroups); // Inverte o arr
+        results.push_back(averages[chosenAvg-1]);
+        qtResults++;
     }
+
+    // print
+    for(int i=0; i < qtResults; i++){
+        std::cout << std::fixed << std::setprecision(1) << results[i] << std::endl;
+    }
+
     return 0;
 }
