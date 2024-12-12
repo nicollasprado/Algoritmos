737a488e443ec83cea81a9cb91aaea4b54dde82b
Nicollas Matheus Prado Pinheiro <20241014040014@ifrn.local>
2024-12-12 11:43:15 -0300

    Continuação da lista 01 de gerenciamento de memoria

---
+++ Gerenciamento_Memoria/lista01/q2/removeElement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+// Finalizar
+
+void removeFirst(std::vector &list, int chosenElement){
+    for(int i = 0; i < list.size(); i++){s}
+}
+
+
+int main(){
+    return 0;
+}
