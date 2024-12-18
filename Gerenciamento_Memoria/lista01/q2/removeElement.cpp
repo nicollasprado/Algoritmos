d4f53c0338bbc0b97eff0093ebba3ad2b413b930
nicollasprado <nicollasprado.profissional@gmail.com>
2024-12-17 21:24:56 -0300

    remove element code

--- Gerenciamento_Memoria/lista01/q2/removeElement.cpp
+++ Gerenciamento_Memoria/lista01/q2/removeElement.cpp
@@ -3,8 +3,15 @@
 
 // Finalizar
 
-void removeFirst(std::vector &list, int chosenElement){
-    for(int i = 0; i < list.size(); i++){s}
+void remove(std::vector<int> &list, int chosenElement){
+    int *p_left = &list[0];
+    int *p_right = &list[list.size()-1];
+    int leftIndex = 0, rightIndex = 0;
+    for(int i = 0; i < list.size(); i++){
+        if(*p_right == chosenElement){
+
+        }
+    }
 }
 
 
